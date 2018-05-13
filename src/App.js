f7c3ecb79b82b366e56a20f59eb015328471725a
natsuking <djyamaexpert@yahoo.co.jp>
2018-05-14 02:01:59 +0900

    add youtubeAPI setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import YSearch from 'youtube-api-search';
 
 import Header from './components/Header/Header';
 
+const YOUTUBE_API_KEY = 'AIzaSyC4Zaowu05ZczH-jEwAhIs3iU1JrgKRNk8'
+
+YSearch({ key: YOUTUBE_API_KEY, term: '猫　きゅうり'}, (data) => {
+  console.log(data);
+});
+
+
 class App extends Component {
-  render() {    
+
+  render() {
     return (
       <div className="App">
         <Header />
