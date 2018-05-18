97ec2a98f78e601c2f364351a77f73039c016efc
natsuking <djyamaexpert@yahoo.co.jp>
2018-05-19 05:44:59 +0900

    add google fonts

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Body = (props) => {
   return (
-    <div>
+    <div className="container-fluid">
       <div className="row">
         {props.children}
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,15 +10,29 @@ class Header extends Component {
   }
 
   render(){
+
+    const fontStyle = {
+      fontFamily: 'Luckiest Guy',
+      fontFamily: 'Bungee'
+    }
+
     return(
-      <div style={{textAlign: 'center'}}>
-        <div>
-          <h2>NekoTube</h2>
-        </div>
-        <div>
-          <input onChange={this.onInputChangeHandler} value={this.state.keyword} />
-        </div>
-      </div>
+      <nav className="navbar navbar-expand
+                    navbar-light bg-light
+                    mb-3 p-3">
+        <h2 className="mb-0" >
+          <span className="bg-dark text-white p-2 mr-2">猫</span>
+          <span className="d-none d-md-inline"
+            style={fontStyle}
+
+            >NekoTube</span>
+        </h2>
+        <form className="form-group my-2 my-lg-0 ml-sm-2 ">
+          <input
+            onChange={this.onInputChangeHandler} value={this.state.keyword}
+            className="form-control form-control-lg mr-sm-2" type="text" placeholder="検索..." aria-label="検索..." />
+        </form>
+      </nav>
     );
   }
 }
